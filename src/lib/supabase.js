36a9569822c08892e20c599548bb124e79ffc9a5
Ali <ali.ash2727@gmail.com>
2026-06-05 15:20:52 +0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
-const URL = import.meta.env.VITE_SUPABASE_URL
-const KEY = import.meta.env.VITE_SUPABASE_KEY
+const SUPABASE_URL = 'https://fvtrwovquavoyakiczrv.supabase.co'
+const SUPABASE_KEY = 'sb_publishable_lELYDMRcDddwfiruCyKxCg_uHAhdhBb'
 
-export const supabase = createClient(URL, KEY)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  }
+})
